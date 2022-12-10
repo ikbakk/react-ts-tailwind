fb54f177bf86e8e8fba7d0d0c86d9265dd90daf7
ikbakkk <iqbalfirdaus05@gmail.com>
2022-12-10 23:13:51 +0800

    fixing eslint

--- src/blocks.tsx
+++ src/blocks.tsx
@@ -4,7 +4,7 @@ type Props ={
   player?: string
 }
 
-function Blocks({ index, onClick, player}: Props) {
+function Blocks ({ index, onClick, player }: Props) {
   const scale = player ? 'scale-100' : 'scale-0'
   const textColor = player === 'X' ? 'text-yellow-200' : 'text-fuchsia-300'
   const hoverStyle = 'transition duration-200 hover:scale-105 transform'
